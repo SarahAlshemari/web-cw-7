b8ee0fe3c270b702de6f9c166b1b72a544d5d9f9
SarahAlshemari <saraalshemari@outlook.com>
2021-01-05 22:10:32 +0300

    Done

--- question1.js
+++ question1.js
@@ -1,1 +1,23 @@
 // قم بحل السؤال الأول هنا
+let pets=[`Dog`,`Cat`,`Hamster`,`Turtle`,`Rabbit`];
+console.log(pets.length);
+pets.pop();
+console.log(pets.length);
+pets.push(`Fish`);
+console.log(pets.length);
+
+pets[2]= "Bird";
+console.log(pets[2])
+
+let Cat=
+   { name: "Pinky" ,
+   age: "2 months" ,
+   breed: "Scottish Fold",
+   };
+
+
+
+Cat.emoji="🐈";
+console.log(Cat.emoji)
+console.log(`the cat's age is ${Cat.age}`);
+
